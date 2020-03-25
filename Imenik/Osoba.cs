4f7d8b586235857a2420bd2442295bba6fcba5bf
Dragan Pepic <d.pepic@hotmail.com>
2020-03-25 19:13:26 +0100

    Add project files.

---
+++ Imenik/Osoba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imenik
+{
+	public class Osoba
+	{
+		public string Ime { get; set; }
+		public string Prezime { get; set; }
+		public string Telefon { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Ime} {Prezime}";
+		}
+	}
+}
---
+++ Imenik/OsobeEditor.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Imenik
+{
+	/// <summary>
+	/// Interaction logic for OsobeEditor.xaml
+	/// </summary>
+	public partial class OsobeEditor : Window
+	{
+		public OsobeEditor()
+		{
+			InitializeComponent();
+			DataContext = new Osoba();
+			BindingGroup = new BindingGroup();
+		}
+
+		private void OK(object sender, RoutedEventArgs e)
+		{
+			if (BindingGroup.CommitEdit() == true)
+			{
+				DialogResult = true;
+				Close();
+			} else
+			{
+				MessageBox.Show("Doslo je do greske :(");
+			}
+		}
+
+		private void Ponisti(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
